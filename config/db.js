58bfd2df338ed5d43fcff6ca53ffeb06fa205e31
yukijina <yukijina@gmail.com>
2020-07-26 22:28:11 -0700

    create and setup a basic route and model

---
+++ config/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config/.env' });
+
+const db = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit rocess with failour
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
