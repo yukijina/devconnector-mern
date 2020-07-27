974574c88f6e3b95d2e764ca93e86ad2035fbda4
yukijina <yukijina@gmail.com>
2020-07-27 16:58:25 -0700

    add experience(embedded in profile)

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -180,4 +180,53 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route PUT api/profile/experience
+// @desc  Add profile experience
+// @access Private
+///** create(put) embedded array */
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('company', 'Company is required').not().isEmpty(),
+      check('from', 'From date is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newExp = { title, company, location, from, to, current, description };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.status(200).json({
+        status: 'success',
+        profile,
+      });
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
 module.exports = router;
