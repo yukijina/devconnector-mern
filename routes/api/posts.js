1cfb8c434b5c1f8765184a9263b489d057a2e791
yukijina <yukijina@gmail.com>
2020-07-28 14:26:49 -0700

    create and delete comments embedded in post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -155,6 +155,7 @@ router.put('/like/:postId', auth, async (req, res) => {
 // *** User is embedded in post
 router.put('/unlike/:postId', auth, async (req, res) => {
   try {
+    // params is set :postId so we get req.params.postId (not just req.params.id)
     const post = await Post.findById(req.params.postId);
     // Check if the post has already been liked
     // video shows .length > 0 after req.user )
@@ -179,4 +180,82 @@ router.put('/unlike/:postId', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route POST api/posts/comment/:id
+// @desc  Create a comment
+// @access Private
+// *** User is embedded in post
+router.post(
+  '/comment/:id', //id is post id
+  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.status(200).json(post.comments);
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route DELETE api/posts/comment/:id(postId)/:commentId
+// @desc  Delete a posts
+// @access Private
+router.delete('/comment/:id/:commentId', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Pull out comment
+    const comment = post.comments.find(
+      (com) => com.id === req.params.commentId
+    );
+
+    // Make sure comment exists
+    if (!comment) {
+      return res.status(404).json({ msg: 'Comment not found' });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(404).json({ meg: 'Comment does not exist' });
+    }
+
+    // Get remove index
+    const removeIndex = post.comments
+      .map((com) => com.user.toString())
+      .indexOf(req.user.id);
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.status(200).json({ msg: 'successfully deleted' });
+  } catch (err) {
+    console.log(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Ppost not found' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
