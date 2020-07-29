cdd6343aa06e848922c9558205dfe19ef88d3c92
yukijina <yukijina@gmail.com>
2020-07-29 14:14:40 -0700

    store and rootreduce setup. crate alert for register page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,11 @@
 import React, { Fragment, useState } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { setAlert } from '../../actions/alert';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+//distrcutuer 'props' to '{setAlert}'
+const Register = ({ setAlert }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -19,7 +23,8 @@ const Register = () => {
     e.preventDefault();
 
     if (password !== password2) {
-      console.log('Password do not match');
+      // setAlert(alert message, alertType)
+      setAlert('Password do not match', 'danger');
     } else {
       console.log('success');
     }
@@ -86,4 +91,8 @@ const Register = () => {
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+};
+
+export default connect(null, { setAlert })(Register);
