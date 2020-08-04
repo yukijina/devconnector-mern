d377d01fe65c9e3bba1508e8be34631515cba6fc
yukijina <yukijina@gmail.com>
2020-08-04 10:16:34 -0700

    get user's profile

---
+++ client/src/actions/profile.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { GET_PROFILE, PROFILE_ERROR } from './type';
+import { setAlert } from './alert';
+
+// Get current user profile
+export const getCurrentProfile = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/profile/me');
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, statu: err.response.status },
+    });
+  }
+};
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import login from './auth';
+import profile from './profile';
 
 export default combineReducers({
   alert,
   auth,
   login,
+  profile,
 });
