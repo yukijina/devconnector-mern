bf87684c23856bdab1488709c468e00377d72318
yukijina <yukijina@gmail.com>
2020-09-21 21:15:02 -0700

    preapare for production (heroku)

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+// path os for production
+const path = require('path');
+
 const connectDB = require('./config/db');
 
 const app = express();
@@ -11,13 +14,25 @@ connectDB();
 // Init Middleware - body parser
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
+// Only for development
+//app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+  // it gose from current directry - client colder - build - index.html
+  // This app has client in the same github
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
